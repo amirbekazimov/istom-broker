a3080e0f1752663966d0f40e97759a3f99c1a6c1
amirbek <amirbekazimov7@gmail.com>
2025-02-14 16:06:40 +0500

    get catalogs for main page

--- src/components/client/HomeCategory.tsx
+++ src/components/client/HomeCategory.tsx
@@ -1,24 +1,60 @@
-import React from "react";
-import CategoryBg from "@/assets/images/Category-bgImage.png";
-import Image from "next/image";
+'use client';
+import React, { useEffect, useState } from 'react';
+
+import CategoryBg from '@/assets/images/Category-bgImage.png';
+import Image from 'next/image';
+
+import { getCatalogs } from '@/services.jsx/catalog';
 
 const HomeCategory = () => {
+  const [catalogs, setCatalogs] = useState([]);
+
+  useEffect(() => {
+    getCatalogs()
+      .then((res) => {
+        console.log('this');
+        console.log(res);
+        setCatalogs(res);
+        console.log('this');
+      })
+      .catch(console.error);
+  }, []);
+
   return (
-    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-      <div className="bg-[#F6F6F6] h-[126px] md:h-[233px] md:rounded-[10px] flex relative overflow-hidden">
-        <div className="absolute right-0 t-0 w-[85%] md:w-full h-full">
+    <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+      {catalogs?.map((catalog: any, index) => (
+        <div
+          key={index}
+          className='bg-[#F6F6F6] h-[126px] md:h-[233px] md:rounded-[10px] flex relative overflow-hidden'
+        >
+          <div className='absolute right-0 t-0 w-[85%] md:w-full h-full'>
+            <Image
+              src={CategoryBg}
+              alt='bg-image'
+              className='object-contain w-full'
+            />
+          </div>
+          <div className='p-2 md:p-4 mt-auto'>
+            <h4 className='text-[14px] break-all md:text-[20px] font-extrabold font-cygre leading-[15px] md:leading-[26px] relative'>
+              {catalog.name}
+            </h4>
+          </div>
+        </div>
+      ))}
+      {/* <div className='bg-[#F6F6F6] h-[126px] md:h-[233px] md:rounded-[10px] flex relative overflow-hidden'>
+        <div className='absolute right-0 t-0 w-[85%] md:w-full h-full'>
           <Image
             src={CategoryBg}
-            alt="bg-image"
-            className="object-contain w-full"
+            alt='bg-image'
+            className='object-contain w-full'
           />
         </div>
-        <div className="p-2 md:p-4 mt-auto">
-          <h4 className="text-[14px] break-all md:text-[20px] font-extrabold font-cygre leading-[15px] md:leading-[26px] relative">
+        <div className='p-2 md:p-4 mt-auto'>
+          <h4 className='text-[14px] break-all md:text-[20px] font-extrabold font-cygre leading-[15px] md:leading-[26px] relative'>
             Стоматотологические материалы
           </h4>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
